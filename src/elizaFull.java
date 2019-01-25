2b83109db69f473ce92efe5b3272ed9c954ab017
Jennifer Lee <jennifer.jlee@outlook.com>
2019-01-25 16:30:54 -0500

    Initial commit

---
+++ src/elizaFull.java
@@ -0,0 +1,122 @@
+/*
+* When the user enters a statement the program responds in one of two ways:
+
+1. With a randomly chosen hedge, such as "Please tell me more"
+
+2. By changing some keywords  in the user's input string an appending this string to a randomly chosen qualifier.
+To get a random item from an array, select a random number for one of the array index values and use the value at that index for your qualifier.*/
+
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
+public class elizaFull {
+
+    public static void main(String[]args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Good day. How are you doing today?");
+
+        String userInput = " ";
+        boolean userQuit;
+        String elizaSays = " ";
+
+        do {
+            System.out.println("Enter your response here, or press Q to quit.");
+            userInput = input.nextLine();
+
+            // Check if user wants to quit
+            userQuit = checkQuitCommand(userInput);
+
+            System.out.println();
+
+            // After user input is read, generate a random number between 0 and 1
+            Random rnd = new Random();
+            int responseOption = rnd.nextInt(2);
+            if(responseOption==1){
+                // call method that randomly chooses hedge
+                elizaSays = respondWithHedges();
+
+            }else{
+                // call method that first replaces words in user input
+                // then prepends the qualifier
+                elizaSays = respondWithQualifiers(userInput);
+            }
+
+            System.out.println(elizaSays);
+            System.out.println();
+
+        }while (!userQuit);
+
+        System.out.println(">>> END");
+        input.close();
+        System.exit(0);
+    }
+
+    public static String respondWithHedges(){
+
+        String[] hedges = {"Please tell me more",
+                "Many of my patients tell me the same thing",
+                "It is getting late, maybe we had better quit"};
+
+        Random rnd = new Random();
+        int index = rnd.nextInt(hedges.length);
+        return hedges[index];
+    }
+
+    public static String respondWithQualifiers(String str){
+
+        String replacedStr = replaceWords(str);
+
+        String[] qualifiers = {"Why do you say that ",
+                "You seem to think that ",
+                "So, you are concerned that "};
+
+        Random rnd = new Random();
+        int index = rnd.nextInt(qualifiers.length);
+        String newStr = qualifiers[index] + replacedStr;
+        return newStr;
+    }
+
+    public static String replaceWords(String str){
+
+        String[] words = str.split(" ");
+        String newStr = "";
+
+        // Use shorthand to loop each element inside array
+        for(String word: words){
+
+            // Lower Case all letters
+            word = word.toLowerCase();
+
+            //    replace i with you
+            //    replace me with you
+            //    replace my with your
+            //    replace am with are
+
+            // If HashMap contains key, swap out key for value
+            HashMap<String, String> myMap = new HashMap<>();
+            myMap.put("i", "you");
+            myMap.put("me", "you");
+            myMap.put("my", "your");
+            myMap.put("am", "are");
+
+            if(myMap.containsKey(word)){
+                word = myMap.get(word);
+            }
+
+            // Concatenate the contents of the array back into a String
+            newStr += word + " ";
+        }
+        return newStr;
+    }
+
+    public static boolean checkQuitCommand(String str){
+
+        boolean quit = false;
+        if (str.equalsIgnoreCase("Q") || str.contains("i am feeling great")){
+            quit = true;
+        }
+        return quit;
+    }
+
+}
